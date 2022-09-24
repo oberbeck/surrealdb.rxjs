cba8989d68af257e2988258d538a7665cee7ef9a
oberbeck <20164654+oberbeck@users.noreply.github.com>
2022-09-24 14:12:13 +0200

    Add SurrealRx

--- packages/reactive/src/lib/reactive.spec.ts
+++ packages/reactive/src/lib/reactive.spec.ts
@@ -1,7 +1,59 @@
-import { reactive } from './reactive';
+import { SurrealRx } from './reactive';
 
 describe('reactive', () => {
-  it('should work', () => {
-    expect(reactive()).toEqual('reactive');
+  it('should work', async () => {
+    const db = new SurrealRx('http://127.0.0.1:8000/rpc');
+
+    db.on$('open').subscribe((e) => console.log('SUBSCRIPTION: open', ...e));
+    db.on$('opened').subscribe((e) =>
+      console.log('SUBSCRIPTION: opened', ...e)
+    );
+    db.on$('close').subscribe((e) =>
+      console.log('SUBSCRIPTION: close', ...e)
+    );
+    db.on$('closed').subscribe((e) =>
+      console.log('SUBSCRIPTION: closed', ...e)
+    );
+    db.on$('notify').subscribe((e) =>
+      console.log('SUBSCRIPTION: notify', ...e)
+    );
+
+    await db.signin({
+      user: 'root',
+      pass: 'root',
+    });
+
+    console.debug('Select a specific namespace / database');
+    await db.use('test', 'test');
+
+    console.debug('Create a new person with a random id');
+    const _created = await db.create('person', {
+      title: 'Founder & CEO',
+      name: {
+        first: 'Tobie',
+        last: 'Morgan Hitchcock',
+      },
+      marketing: true,
+      identifier: Math.random().toString(36).substr(2, 10),
+    });
+
+    console.debug('Update a person record with a specific id');
+    const _updated = await db.change('person:jaime', {
+      marketing: true,
+    });
+
+    console.debug('Select all people records');
+    const _people = await db.select('person');
+
+    console.debug('Perform a custom advanced query');
+    const _groups = await db.query(
+      'SELECT marketing, count() FROM type::table($tb) GROUP BY marketing',
+      {
+        tb: 'person',
+      }
+    );
+
+    console.debug('closing');
+    await db.close();
   });
 });
--- packages/reactive/src/lib/reactive.ts
+++ packages/reactive/src/lib/reactive.ts
@@ -1,3 +1,22 @@
-export function reactive(): string {
-  return 'reactive';
+import { withObservables } from '@surrealdb.rxjs/reactive-surreal-emitter';
+import Surreal from 'surrealdb.js';
+
+/**
+ * Surreal with Reactive Extensions
+ */
+export class SurrealRx extends withObservables(Surreal) {
+  static #singleton?: SurrealRx;
+
+  /**
+   * @note this is *not* the same instance as `Surreal.Instance`
+   */
+  static override get Instance(): Surreal {
+    return this.#singleton
+      ? this.#singleton
+      : (this.#singleton = new SurrealRx());
+  }
+
+  static override get Live() {
+    return withObservables(super.Live);
+  }
 }
